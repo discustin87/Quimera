b83fdadff44d51fe3489fd3d4774f3886ca325fb
Justin Croteau <justinwebdev1@gmail.com>
2021-11-12 14:18:34 -0600

    !

--- stylingScript.js
+++ stylingScript.js
@@ -125,3 +125,49 @@ function removeShow() {
 tabItems.forEach(item => {
 	item.addEventListener('click', selectItem);
 });
+
+
+// **************** home slider ***************
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+const imgs = document.getElementById('imgs');
+
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0;
+
+let interval = setInterval(run, 10000)
+
+function run() {
+    idx++ 
+    changeImage();
+}
+
+function changeImage() {
+    if (idx > img.length - 1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 600}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(run, 10000)
+}
+
+if (nextBtn !== null && prevBtn !== null) {
+  nextBtn.addEventListener('click', () => {
+    idx++
+    changeImage();
+    resetInterval()
+});
+
+prevBtn.addEventListener('click', () => {
+    idx--
+    changeImage();
+    resetInterval()
+});
+}
