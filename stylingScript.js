9707311157520c8f2fc6ff1f6561ba4596cd89a8
Justin Croteau <justinwebdev1@gmail.com>
2021-11-18 13:18:52 -0600

    1

--- stylingScript.js
+++ stylingScript.js
@@ -136,7 +136,7 @@ const img = document.querySelectorAll('#imgs img')
 
 let idx = 0;
 
-let interval = setInterval(run, 10000)
+let interval = setInterval(run, 3000)
 
 function run() {
     idx++ 
